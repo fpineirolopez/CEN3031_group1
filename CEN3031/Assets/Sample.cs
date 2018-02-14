50ddfcbf9570124c0ad1d64171572b9af8ba32eb
Nozomi Watanabe <27590679+kmakoto02@users.noreply.github.com>
2018-02-14 15:24:05 -0500

    Added sample C# file

---
+++ CEN3031/Assets/Sample.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//For the moment, see if you can add your name to this c# file:
+//Nozomi Watanabe
+
+
+public class Sample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
