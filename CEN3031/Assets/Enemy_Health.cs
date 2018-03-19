93f2c1361c3f1924c58bf6610a9046d08204241a
mooneyj01 <mooneyj@ufl.edu>
2018-03-19 09:11:33 -0400

    Enemy AI

---
+++ CEN3031/Assets/Enemy_Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Health : MonoBehaviour {
+
+    /// Total hitpoints
+    public int hp = 5;
+
+    /// Enemy or player
+    public bool isEnemy = true;
+
+    /// Inflicts damage and check if the object should be destroyed
+    public void Damage(int damageCount)
+    {
+        hp -= damageCount;
+
+        if (hp <= 0)
+        {
+            // Dead!
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
