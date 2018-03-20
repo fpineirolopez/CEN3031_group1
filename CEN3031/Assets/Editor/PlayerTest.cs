3c08c3afdf28c2f94ca584ace4f4f2720620e51e
Mitchell Joppru <mjoppru@ufl.edu>
2018-03-20 09:13:25 -0400

    Refactored player code, Added test code for player movement
    Refactored the player code to separate unity dependencies from player movement logic.  Added test code to test the logic of creating the player's movement vector.

---
+++ CEN3031/Assets/Editor/PlayerTest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools.Utils;
+
+public class PlayerTest
+{
+
+    [Test]
+    public void PlayerTestSimplePasses()
+    {
+        //Initialize player controller object
+        Player_control pctrl = new Player_control();
+
+        //Create vector to mime inputs
+        Vector2 inputs = new Vector2(0.0f, 0.0f);
+        Vector2 movement = new Vector2(0.0f, 0.0f);
+
+        //Testing for the movement vector calculator for all possible inputs
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                inputs = new Vector2(-1.0f + i, -1.0f + j);
+                movement = pctrl.movement_calculator(inputs.x, inputs.y);
+                inputs.Normalize();
+                Assert.AreEqual(movement.x, inputs.x);
+                Assert.AreEqual(movement.y, inputs.y);
+            }
+        }
+
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator PlayerTestWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
+
--- CEN3031/Assets/Player_control.cs
+++ CEN3031/Assets/Player_control.cs
@@ -2,24 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_control : MonoBehaviour
+public class Player_control
 {
-    private Rigidbody2D rb2d;
-    public float speed;
 
-    void Start()
+    public Vector2 movement_calculator(float moveHorizontal, float moveVertical)
     {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
-    private void FixedUpdate()
-    {
-        
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.MovePosition( rb2d.position + movement * speed * Time.fixedDeltaTime );
-
+        movement.Normalize();
+        return movement;
     }
 
 
 }
+
