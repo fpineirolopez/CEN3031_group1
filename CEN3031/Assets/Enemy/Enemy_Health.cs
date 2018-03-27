db67dc4b696d2a28e414423ad7032eadd93b0c5f
fpineirolopez <pineiro.felipe@gmail.com>
2018-03-27 04:03:38 -0400

    Revert "Basic combat"
    This reverts commit 0d50cf2d11ea18ad346d0dc56ec3848c06075240, reversing
    changes made to 6dda1419ccd17d209854c27aca4a3055dc93cad8.

--- CEN3031/Assets/Enemy/Enemy_Health.cs
+++ CEN3031/Assets/Enemy/Enemy_Health.cs
@@ -22,21 +22,5 @@ public class Enemy_Health : MonoBehaviour {
         }
     }
 
-    void OnTriggerEnter2D(Collider2D otherCollider)
-    {
-        // Is this a shot?
-        Shot shot = otherCollider.gameObject.GetComponent<Shot>();
-        if (shot != null)
-        {
-            // Avoid friendly fire
-            if (shot.isEnemyShot != isEnemy)
-            {
-                Damage(shot.dmg);
-
-                // Destroy the shot
-                Destroy(shot.gameObject); // Remember to always target the game object, otherwise you will just remove the script
-            }
-        }
-    }
-
+    
 }
