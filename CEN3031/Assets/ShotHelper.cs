4517f2f711d94c3456e6c34699b524f828c3f2b7
Nicholas Young <ndy14@ufl.edu>
2018-04-10 02:11:32 -0400

    Shot unit test
    Added super basic and totally unnecessary helper class for super basic and totally unnecessary unit test for player's shot

---
+++ CEN3031/Assets/ShotHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotHelper {
+
+    // Test class for ensuring timer is correct
+    public int dmg;
+    public float range;
+    public float shot_speed;
+
+    public float getTimer() {
+        return range / shot_speed;
+    }
+}
