fa9dc25a215dfcf40702b3fd2a06a51081f2794f
fpineirolopez <pineiro.felipe@gmail.com>
2018-04-14 14:53:46 -0400

    Added FadeIn and FadeOut animations between MainMenu and GameOver

--- CEN3031/Assets/Menus/GameOverMenu.cs
+++ CEN3031/Assets/Menus/GameOverMenu.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverMenu : MonoBehaviour {
+
+    public Image black;
+    public Animator anim;
 
     /*Function triggers returning to the main menu*/
     public void ReturntoMainMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        StartCoroutine(Fading());
     }
 
     /*Function triggers quitting the game and exits the application. I added the Debug message to make sure that
@@ -17,6 +21,13 @@ public class GameOverMenu : MonoBehaviour {
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene("MainMenu");
     }
 
 }
