da9dae5cb408f3ab9cb079750457040eecfc20ef
mooneyj01 <mooneyj@ufl.edu>
2018-04-14 16:05:56 -0400

    Enemy Collision Damage and Player Damage Timer

--- CEN3031/Assets/Player_Health.cs
+++ CEN3031/Assets/Player_Health.cs
@@ -8,7 +8,10 @@ public class Player_Health : MonoBehaviour
 
     public int max_hp;          // set in unity, in the player asset itself
     public int current_hp;     // current health, used for calculations
-    bool isDead = false;        // checks when player dies
+    bool isDead = false;   // checks when player dies
+    float Damage_timer;
+    public bool Can_Take_Damage;
+    public float Damage_Cooldown;
 
     public Health phealth = new Health(); // for offloading calculations to a testable interface
 
@@ -22,22 +25,47 @@ public class Player_Health : MonoBehaviour
         //Initialize with full health bar, setting the max value to whatever number we set the max health in the player asset
         health_bar.maxValue = max_hp;
         health_bar.value = current_hp;
+        Can_Take_Damage = true;
+        Damage_timer = .5f;
 
     }
 
+    void Update()
+    {
+        if (!Can_Take_Damage)
+        {
+            Damage_timer -= Time.deltaTime;
+            if(Damage_timer <= 0)
+            {
+                Can_Take_Damage = true;
+                Damage_timer = .5f;
+            }
+           
+        }
+        
+    }
     // Inflicts damage and check if the object should be destroyed
     public void Damage(int damageCount)
     {
-        //Get the health int based on input
-        current_hp = phealth.health_calculator(current_hp, damageCount);
-
-        health_bar.value = current_hp;  //update healthbar
-
-        if (current_hp <= 0 && !isDead)
+        if (Can_Take_Damage)
         {
-            // Dead!
-            PlayerDies();
+            //Get the health int based on input
+            current_hp = phealth.health_calculator(current_hp, damageCount);
+
+            health_bar.value = current_hp;  //update healthbar
+
+            if (current_hp <= 0 && !isDead)
+            {
+                // Dead!
+                PlayerDies();
+            }
+            Can_Take_Damage = false;
         }
+        else
+        {
+            //do nothing
+        }
+        
     }
 
     void OnTriggerEnter2D(Collider2D otherCollider)
