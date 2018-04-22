7e58a9843f217ed4ec7c1d2e7273e2a0f87f64ec
fpineirolopez <pineiro.felipe@gmail.com>
2018-04-22 14:44:30 -0400

    A Lot of things, check description
    updated boss prefab so it has the animations and the death splatter.
    Moved fade-in and fade-out animations and controller to the animations folder.
    Added text to display the level, and the score, and scripts to control them. Known bugs with the score is that it resets after every floor. To get scores for bugs and mages, copied the enemy_health script, renamed it enemy_health_bug and attached it to the bug. This way I could assign different values to the bugs and the mages when we kill them.

---
+++ CEN3031/Assets/LevelText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelText : MonoBehaviour
+{
+    public Text level_number; //initilaize
+
+    void Start()
+    {   
+        level_number.text = "1"; //set it to 1 when starting
+    }
+
+    void Update()
+    {
+        int level = GameObject.Find("Level Generator").GetComponent<LevelGeneration>().Get_Level(); //get level number from level generation script
+        level_number.text = level.ToString(); //change it to new level
+    }
+}
